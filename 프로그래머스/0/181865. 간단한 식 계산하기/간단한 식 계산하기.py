f4eb00b5cfaeed1662adea030a55ab11830e9dcd
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2024-07-02 14:44:53 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,10 @@
+def solution(binomial):
+    
+    x = binomial.split()
+    
+    if x[1] == '+':
+        return int(x[0])+int(x[2])
+    elif x[1] == '-':
+        return int(x[0])-int(x[2])
+    elif x[1] == '*':
+        return int(x[0])*int(x[2])
