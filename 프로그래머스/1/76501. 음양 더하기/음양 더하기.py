dba5c22c2843014baedfc0cd1a1cf3c23cd41d3a
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2024-08-20 17:19:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = []
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer.append(absolutes[i])
+        else:
+            answer.append(-absolutes[i])
+    return sum(answer)
