58909b502849012e9393437553f5e658bf561db5
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2024-10-22 14:29:56 +0900

    [level 2] Title: H-Index, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,8 @@
 def solution(citations):
-    answer = 0
     citations.sort(reverse=True)
-    
-    for i in range(len(citations)):              # H_index가 존재하고 H_index를 넘는 논문이 몇 개인지 구할때
-        if(citations[i] < i+1):
+    for i in range(len(citations)):
+        if citations[i] < i+1 :
             return i
-
-    return len(citations)   
+            
+    
+    return len(citations)
