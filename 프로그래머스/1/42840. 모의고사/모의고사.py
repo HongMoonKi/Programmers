7ae4227f54236adae1b4fcb22f7f301485480e2a
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2024-10-24 01:05:10 +0900

    [level 1] Title: 모의고사, Time: 2.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,22 @@
 def solution(answers):
-    
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
-    
-    score = [0,0,0]
+    a = [1,2,3,4,5]
+    b = [2,1,2,3,2,4,2,5]
+    c = [3,3,1,1,2,2,4,4,5,5]
+    a1,b1,c1 = 0,0,0
+
+    for i in range(len(answers)):
+        if answers[i] == a[(i%5)]:
+            a1 += 1
+        if answers[i] == b[(i%8)]:
+            b1 += 1
+        if answers[i] == c[(i%10)]:
+            c1 += 1
+    k=max(a1,b1,c1)
     result = []
-    
-    for idx, answer in enumerate(answers):
-    	if answer == man1[idx%len(man1)]:
-        	score[0] += 1
-    	if answer == man2[idx%len(man2)]:
-        	score[1] += 1
-    	if answer == man3[idx%len(man3)]:
-        	score[2] += 1   
-    for idx, s in enumerate(score):
-    	if s == max(score):
-        	result.append(idx+1)
-    return result
+    if k == a1:
+        result.append(1)
+    if k == b1:
+        result.append(2)
+    if k == c1:
+        result.append(3)
+    return result    
