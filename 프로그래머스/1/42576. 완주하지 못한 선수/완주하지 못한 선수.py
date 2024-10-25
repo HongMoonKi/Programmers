07f8144dfad65104c63478846b72952e2fd66662
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2024-10-25 22:32:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.93 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,17 @@
 def solution(participant, completion):
+    hashDict = {}
+    sumHash = 0
     
-    participant.sort()
-    completion.sort()
+    # 1. Hash : Participant의 dictionary 만들기
+    # 2. Participant의 sum(hash) 구하기
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
     
-    for i in range(len(participant) - 1):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1] 
+    # 3. completion의 sum(hash) 빼기
+    for comp in completion:
+        sumHash -= hash(comp)
+    
+    # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다
+
+    return hashDict[sumHash]
