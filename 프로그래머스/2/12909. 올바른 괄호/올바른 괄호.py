c4d23b30892e217af5a2b12eebf95da62d18ea84
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2025-03-26 17:14:28 +0900

    [level 2] Title: 올바른 괄호, Time: 5.80 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,11 @@
 def solution(s):
     count = 0
-    for char in s:
-        if char == '(':
+    for i in s:
+        if i == '(':
             count += 1
-        elif char == ')':
-            count -= 1
-        
-        # 닫는 괄호가 먼저 나오면 잘못된 경우
-        if count < 0:
-            return False
-    
-    # 모든 괄호가 짝이 맞아야만 올바른 괄호
+        else:
+            if count == 0 :
+                return False
+            else:
+                count -= 1
     return count == 0
