4b3eac191fe5f8ccad266c41caa47dc4f36b3bc3
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2025-09-11 10:20:40 +0900

    [level 2] Title: 숫자의 표현, Time: 0.59 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,2 +1,8 @@
 def solution(n):
-    return len([i  for i in range(1,n+1,2) if n % i is 0])
+    answer = 0
+    # 1부터 n까지의 수 중에서
+    for i in range(1, n + 1):
+        # n의 약수이면서 동시에 홀수인 경우를 찾는다.
+        if n % i == 0 and i % 2 == 1:
+            answer += 1
+    return answer
