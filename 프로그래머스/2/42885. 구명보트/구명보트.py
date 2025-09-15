180da23f91703ecd0422090822a803509b846255
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2025-09-15 12:55:09 +0900

    [level 2] Title: 구명보트, Time: 8.77 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,12 +1,15 @@
-def solution(people, limit) :
+def solution(people, limit):
+    people.sort()  # 오름차순 정렬
     answer = 0
-    people.sort()
-
-    a = 0
-    b = len(people) - 1
-    while a < b :
-        if people[b] + people[a] <= limit :
-            a += 1
-            answer += 1
-        b -= 1
-    return len(people) - answer
+    left = 0
+    right = len(people) - 1
+    
+    while left <= right:
+        # 가장 가벼운 사람 + 가장 무거운 사람
+        if people[left] + people[right] <= limit:
+            left += 1  # 둘이 같이 타니까 가벼운 사람도 처리
+        # 무거운 사람은 무조건 타야 함
+        right -= 1
+        answer += 1
+        
+    return answer
