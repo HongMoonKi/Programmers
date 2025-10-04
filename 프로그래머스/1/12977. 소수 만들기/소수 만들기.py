749c4d98cf4126ca9cddea497fc9473c26afcdb1
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2025-10-04 09:39:37 +0900

    [level 1] Title: 소수 만들기, Time: 199.30 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,19 +1,13 @@
+from itertools import combinations
+
 def solution(nums):
-    answer=[]
-    result=0
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            for k in range(j+1,len(nums)):
-                answer.append(nums[i] + nums[j] + nums[k])
-    for b in answer:
-        if b > 1:  # 1보다 큰 수만 소수 검사
-            is_prime = True
-            for i in range(2, int(b**0.5) + 1):
-                if b % i == 0:
-                    is_prime = False
-                    break
-            if is_prime:
-                result += 1
-    return result
-            
+    answer = 0
+    for i in combinations(nums,3):
+        cand = sum(i)
         
+        for j in range(2,cand):
+            if cand % j == 0:
+                break
+        else:
+            answer += 1
+    return answer
