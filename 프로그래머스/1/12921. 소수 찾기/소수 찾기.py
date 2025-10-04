65e022201f2e93fe4b766713b64ff7a8645b0815
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2025-10-04 13:30:05 +0900

    [level 1] Title: 소수 찾기, Time: 440.76 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,7 @@
 def solution(n):
-    result = 0
-    for i in range(2, n+1):  # 2부터 n까지 확인
-        is_prime = True
-        for j in range(2, int(i**0.5)+1):  # 2부터 √i까지 확인
-            if i % j == 0:  # 나누어떨어지면 소수가 아님
-                is_prime = False
-                break
-        if is_prime:  # 소수인 경우
-            result += 1
-    return result
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
