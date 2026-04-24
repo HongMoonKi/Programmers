d1ce7ee76c3c53ef2095005a6e2d205f30eb3077
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-04-24 17:17:00 +0900

    [level 1] Title: 약수의 합, Time: 0.51 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,8 @@
 def solution(n):
     answer = 0
-    
-    for i in range(1,n+1):
-        if n%i == 0:
-            answer += i
+    a = 1
+    while a <= n:
+        if n%a == 0:
+            answer = answer + a
+        a = a+1
     return answer
