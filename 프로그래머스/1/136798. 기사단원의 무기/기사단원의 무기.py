36b2b318ce11c79ba8d63cb6fa65bf5817015939
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-04-27 13:45:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 1071.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,9 +1,19 @@
-def cf(n): # 공약수 출력
-    a = []
-    for i in range(1,int(n**0.5)+1):
-        if n%i == 0:
-            a.append(n//i)
-            a.append(i)
-    return len(set(a))
-def solution(number, limit, power):
-    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
+def solution(number,limit,power):  
+    result = []
+    real = 0
+    for i in range(1,number+1):
+        answer = 0
+        for j in range(1, int(i**0.5) + 1):
+            if i % j == 0:
+                if j == i // j:
+                    answer += 1
+                else:
+                    answer += 2
+        result.append(answer)
+    for k in result:
+        if k <= limit:
+            real += k
+        else:
+            real += power
+    return real
+                
