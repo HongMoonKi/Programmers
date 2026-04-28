4c9cce6c28643f484927aebb3d1e2d916f6bfa37
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-04-28 18:59:54 +0900

    [level 2] Title: 올바른 괄호, Time: 11.66 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,9 @@
 def solution(s):
-    
-    stack = []
-    
-    for i in s:
-        if i == '(':
-            stack.append('i')
-        
+    answer = []
+    answer.append(s[0])
+    for i in range(1,len(s)):
+        if answer and s[i] == ")" and answer[-1] == "(":
+            answer.pop()
         else:
-            if stack == []:
-                return False
-            else:
-                stack.pop()
-
-    if stack != []:
-        return False
-    
-    return True
+            answer.append(s[i])
+    return True if len(answer)==0 else False
