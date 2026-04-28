192edfe8ab5488072ca2e828b5ad09ffe7b4c02d
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-04-28 21:35:34 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,10 @@
 def solution(brown, yellow):
     answer = []
-    total = brown + yellow
-    for b in range(1, total+1):
-        if(total/b) % 1 == 0:
-            a = total/b
-            if a >= b:
-                if a*2 + 2*b == brown +4:
-                    return [a,b]
-    return answer
+    total = brown+yellow
+
+    for i in range(1,total+1):
+        if total%i == 0:
+            w = total//i
+            h = i
+            if (w - 2) * (h - 2) == yellow:
+                return [w,h]
