21f92e02ab050fec93e8b41ef3801417676f0d05
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-09-03 18:36:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.79 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,7 @@
 def solution(arr):
     x = []
     for i in arr:
-        if len(x) == 0:
-            x.append(i)
-        elif x[-1] != i:
+        if len(x) == 0 or x[-1] != i:
             x.append(i)
     return x
+            
