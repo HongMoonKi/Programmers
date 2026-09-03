ca22a4a1a0b87e27f5f0c95dd7f5a2aff45e6aa1
HongMoonKi <94909005+HongMoonKi@users.noreply.github.com>
2026-09-03 20:27:54 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,8 +1,28 @@
+from collections import deque
+
 def solution(progresses, speeds):
-    Q=[]
-    for p, s in zip(progresses, speeds):
-        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
-            Q.append([-((p-100)//s),1])
-        else:
-            Q[-1][1]+=1
-    return [q[1] for q in Q]
+    day = []
+
+
+    for i, j in zip(progresses, speeds):
+        a = 1
+
+        while (100 - i) > j * a:
+            a += 1
+
+        day.append(a)
+
+    q = deque(day)
+    answer = []
+
+    while q:
+        standard = q.popleft()
+        count = 1
+
+        while q and q[0] <= standard:
+            q.popleft()
+            count += 1
+
+        answer.append(count)
+
+    return answer
